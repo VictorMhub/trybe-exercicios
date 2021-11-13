a46e3e9914cd6a256fece5e76f258c17d0d7aa10
Victor <victormfseveriano@hotmail.com>
2021-11-12 22:51:25 -0300

    zzz

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.teste.js
+++
@@ -1,5 +0,0 @@
-const myFizzBuzz = require("./myFizzBuzz");
-
-it("Verifica se recebendo 3 e 5 retorna 'fizzbuzz"), ()=>{
-    expect(myFizzBuzz(3,5)).toBe('fizzbuzz')
-}
--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,17 +1,37 @@
 
-const sum = require('./sum')
 
-it('A função recebe 4 e 5 e retorna 9',()=>{
-    expect(sum(4,5)).toBe(9);
+
+const sum = require('./sum.js');
+
+describe('testa a função sum', () => {
+  it('deve retornar o resultado da soma', () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+  });
+  it('deve disparar um erro caso receba como parâmetro uma string', () => {
+    expect(() => sum(4, '5')).toThrowError();
+    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+  })
 })
 
-it('A função recebe 0 e 0 e retorna 0',()=>{
-   expect(sum(0,0)).toBe(0);
-})
+const myFizzBuzz = require('./sum.js');
 
-test("Verificar se a função sum retorna 'parameters must be numbers' quando recebe 4 e '5'", () => {
-    // Uso da Arrow Function para pegar a quebra do fluxo da chamada da função
-    expect(() => {
-      sum(4,'5');
-    }).toThrow('parameters must be numbers');
+describe('testa a função myFizzBuzz', () => {
+  it('verifica, se de acordo com o parâmetro recebido, retorna o valor esperado', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+    expect(myFizzBuzz(3)).toBe('fizz');
+    expect(myFizzBuzz(5)).toBe('buzz');
+    expect(myFizzBuzz(1)).toBe(1);
+    expect(myFizzBuzz('2')).toBe(false);
+  })
+});
+  
+const myRemove = require('./sum.js');
+
+describe('testa a função myRemove', () => {
+    it('deve retornar um array removendo o item correto', () => {
+      expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+      expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+      expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
   });
